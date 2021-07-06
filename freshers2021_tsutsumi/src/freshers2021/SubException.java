c38caf112c88633ee3908dc31486e749c89b7325
Tsutsumi Saaya <Saaya_Tsutsumi@jmas.co.jp>
2021-07-06 18:27:40 +0900

    独自例外処理不明点記載

--- freshers2021_tsutsumi/src/freshers2021/SubException.java
+++ freshers2021_tsutsumi/src/freshers2021/SubException.java
@@ -1,7 +1,7 @@
 package freshers2021;
 
 public class SubException extends Exception{
-	 
+	//TODO L4‚©‚çÅŒã‚Ü‚Å‘‚«•û‚ª‚í‚©‚ç‚È‚¢ 
 	private static final long serialVersionUID = 1L; 
 
         public SubException() { }
