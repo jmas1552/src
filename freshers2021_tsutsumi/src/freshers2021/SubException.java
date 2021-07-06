c84a68fdbdb7db767ca9e608b8aaf6c392fc349f
Tsutsumi Saaya <Saaya_Tsutsumi@jmas.co.jp>
2021-07-06 17:27:18 +0900

    独自例外処理挑戦記録

--- freshers2021_tsutsumi/src/freshers2021/SubException.java
+++ freshers2021_tsutsumi/src/freshers2021/SubException.java
@@ -1,11 +1,10 @@
 package freshers2021;
 
 public class SubException extends Exception{
-		//warningを回避するための宣言
-		private static final long serialVersionUID = 1L; 
+	 
+	private static final long serialVersionUID = 1L; 
 
-		// コンストラクタ
-		SubException(String msg){
-			super(msg);
-		}
-	}
+        public SubException() { }
+                super(cause);
+        }
+}
