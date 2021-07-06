eed14528c9da9509c03d829902b6968997bd1895
Tsutsumi Saaya <Saaya_Tsutsumi@jmas.co.jp>
2021-07-06 11:30:51 +0900

    try文を節分アルゴリズムのメソッド１つめにも追加

--- freshers2021_tsutsumi/src/freshers2021/JpnSpringFestival.java
+++ freshers2021_tsutsumi/src/freshers2021/JpnSpringFestival.java
@@ -10,9 +10,8 @@ class JpnSpringFestival {
 	public static final String names[] = new String[6];
 	public static final int beansNum[] = new int[6];
 	Scanner stdIn = new Scanner(System.in);
-	
+
 	public void algorithm() {
-		
 
 		names[0] = "じいちゃん";
 		names[1] = "ばあちゃん";
@@ -20,43 +19,51 @@ class JpnSpringFestival {
 		names[3] = "かあちゃん";
 		names[4] = "おれ";
 		names[5] = "いもうと";
-		
-		//プログラム表題出力
+
+		// プログラム表題出力
 		System.out.println("＊＊＊＊＊＊＊＊＊＊＊");
 		System.out.println("節分アルゴリズムプログラム");
 		System.out.println("＊＊＊＊＊＊＊＊＊＊＊");
-		
-		//節分の日か
-		System.out.println("今日は節分ですか(y/n)");
-		String answer = stdIn.next();
 
-		// 節分でない場合はサービス終了
-		if (NO.equals(answer)) {
-			System.out.println("本日は節分の日ではないので、終了します。");
-			
-		//節分の日の対応	
-		} else if (YES.equals(answer)) {
-			startAlgorithm();
-			
-		//選択肢以外の入力があった際はエラーメッセージ表示
-		}else {
-			MainProgram.messageInputMismatch() ;
+		// 節分の日か
+		System.out.println("今日は節分ですか(y/n)");
+		try {
+			String answer = stdIn.next();
+
+			// 節分でない場合はサービス終了
+			if (NO.equals(answer)) {
+				System.out.println("本日は節分の日ではないので、終了します。");
+
+				// 節分の日の対応
+			} else if (YES.equals(answer)) {
+				startAlgorithm();
+
+				// 選択肢以外の入力があった際はエラーメッセージ表示
+			} else {
+				MainProgram.messageInputMismatch();
+				System.exit(1);
+			}
+			// 識別子に対応しない入力が発生した際は例外処理する
+		} catch (InputMismatchException e) {
+			MainProgram.messageInputMismatch();
 			System.exit(1);
 		}
 	}
-	//節分の日アルゴリズム開始
+
+	// 節分の日アルゴリズム開始
 	public void startAlgorithm() {
 
 		try {
-			//各人の年齢入力を要求
+			// 各人の年齢入力を要求
 			for (int i = 0; i < beansNum.length; i++) {
 				System.out.println(names[i] + "の年齢");
 				beansNum[i] = stdIn.nextInt();
 				if (beansNum[i] > 100) {
+					MainProgram.messageInputMismatch();
 					System.exit(1);
 				}
 			}
-			//各人の食べた豆を表示
+			// 各人の食べた豆を表示
 			for (int i = 0; i < beansNum.length; i++) {
 				System.out.print("＊＊＊" + names[i] + "は豆を");
 				System.out.println(beansNum[i] + "粒食べました＊＊＊");
@@ -66,30 +73,31 @@ class JpnSpringFestival {
 			String beans200Piaces = null;
 			for (int j = 0; j < beansNum.length; j++) {
 				sum += beansNum[j];
-				
-				//100個目の豆を食べた人表示
+
+				// 100個目の豆を食べた人表示
 				if (beans100Piaces == null && sum >= 100) {
 					beans100Piaces = names[j];
 					System.out.println("100粒目の豆を食べたひとは" + names[j]);
-					
-				//200個目の豆を食べた人表示
+
+					// 200個目の豆を食べた人表示
 				} else if (beans200Piaces == null && sum >= 200) {
 					beans200Piaces = names[j];
 					System.out.println("200粒目の豆を食べたひとは" + names[j]);
 				}
 			}
-			if(sum<100) {
+			if (sum < 100) {
 				System.out.println("100粒目の豆を食べたひとはいません。");
-			}if(sum<200) {
+			}
+			if (sum < 200) {
 				System.out.println("200粒目の豆を食べたひとはいません。");
 			}
-			//家族全員が食べた豆の合計を表示
+			// 家族全員が食べた豆の合計を表示
 			System.out.print("家族全員で食べた豆の数は、");
 			System.out.println(sum + "です");
-			
-			//識別子に対応しない入力が発生した際は例外処理する
+
+			// 識別子に対応しない入力が発生した際は例外処理する
 		} catch (InputMismatchException e) {
-			MainProgram.messageInputMismatch() ;
+			MainProgram.messageInputMismatch();
 			System.exit(1);
 		}
 	}
