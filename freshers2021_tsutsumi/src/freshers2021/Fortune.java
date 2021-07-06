b6f315ce11351bfe3c70ee3a12506c8f7c5dcd8d
Tsutsumi Saaya <Saaya_Tsutsumi@jmas.co.jp>
2021-07-06 17:50:46 +0900

    一旦完成（例外処理抜きで）

--- freshers2021_tsutsumi/src/freshers2021/Fortune.java
+++ freshers2021_tsutsumi/src/freshers2021/Fortune.java
@@ -1,45 +1,46 @@
 package freshers2021;
 
+
 import java.util.Scanner;
 import java.util.Random;
 
 import java.util. InputMismatchException;
 
-public void today(){
+class Fortune{
+	
 
-	private final Random rand = new Random();
 	public static final String BEST_HAPPY="大吉です。おめでとうございます！";
 	public static final String SECOND_HAPPY="吉です。";
 	public static final String MIDDLE_HAPPY="中吉です。";
 	public static final String NEXT_TO_WORST_HAPPY="小吉です";
 	public static final String WORST_HAPPY="凶です。ファイト！！";
-	String name;
+	public static  String name;
 	Scanner stdIn=new Scanner(System.in);
 	Random rand=new Random();
 
-	
-	public static void algorithm(){
+
+	public  String main(String[]args){ //一旦メインメソッド
+
+
 		
 			//乱数設定
- 		int rnd = rand(10);
+ 		int rnd = rand.nextInt(10);
  			 if(0 <= rnd && rnd < 1) {
+ 			 // 大吉
 			 	return BEST_HAPPY;
   			}else if(1 < rnd && rnd <= 2) {
-    		// 大吉
-  				return SECOND_HAPPY;
-  			}else if(2 < rnd && rnd <= 4){
     		// 吉
-   				return MIDDLE_HAPPY;	
-  			}else if(4 < rnd && rnd <= 6){
+  				return SECOND_HAPPY;
+  			}else if(2 < rnd && rnd <= 6){
     		// 中吉
-   				return NEXT_TO_WORST_HAPPY;
+   				return MIDDLE_HAPPY;	
   			}else if(6 < rnd && rnd <= 8){
-			// 小吉
-   				return eItem_CopperCoin;
+    		// 小吉
+   				return NEXT_TO_WORST_HAPPY;
   			}else if(8 < rnd && rnd <= 10){
     		// 凶
    				return WORST_HAPPY;
-		}
+  			}
 		
 		System.out.println("＊＊＊＊＊＊＊＊＊＊＊");
 		System.out.println("今日の運勢プログラム");
@@ -50,14 +51,13 @@ public void today(){
 		
 		if(name==null){
 			//まだ条件設定完了していない
-			String nameAgain;
 			for(int i=0; i<3; i++){
 				System.out.println("あなたの名前を入力してください");
-					String nameAgain=stdIn.next();
-				if(nameAgain != null){
+					name=stdIn.next();
+				if(name != null){
 					continue;
-				}
-			}name=nameAgain;
+				}//処理終了
+			}
 		}
 		System.out.print(name + "さんの今日の運勢は、");
 		System.out.println(rnd);
